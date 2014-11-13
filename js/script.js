14c58a367539f34bd5c968f6fd4f59a10af4d2a9
Gabriele Pallaoro <gabriele.pallaoro@gmail.com>
2014-11-13 18:11:47 +0100

    random rotate

--- js/script.js
+++ js/script.js
@@ -4,8 +4,16 @@
  * and open the template in the editor.
  */
 $(document).ready(function () {
+    
+    var numqudrati=150;
+    
+    setInterval(function () {
+        var r=Math.floor((Math.random() * numqudrati));
+        $(".quadrato").eq(r).mouseenter();
+    }, 1000);
+    
     var vettore = ["#00AACC", "#FF4E00", "#00dd2f", "#ff4444", "#00AACC", "#ffdb00", "#a700b3"];
-    for (var i=0; i < 150; i++) {
+    for (var i = 0; i < 150; i++) {
         var col = vettore[Math.floor((Math.random() * vettore.length))];
         var $append = $(".append");
         $append.after("<div class=\"quadrato\" style=\"background-color:" + col + "\"></div>");
@@ -13,16 +21,16 @@ $(document).ready(function () {
 
     $(document).on("click", ".quadrato", function (e) {
         $(this).fadeOut("slow");
+        numqudrati--;
     });
     $(".button").click(function () {
-        //for (var i=0; i < 10; i++) {
+        numqudrati++;
         var col = vettore[Math.floor((Math.random() * vettore.length))];
         var $append = $(".append");
         $append.after("<div class=\"quadrato\" style=\"background-color:" + col + "\"></div>");
-        //}
     });
     $(document).on("mouseenter", ".quadrato", function (e) {
-        $(this).animate({borderSpacing: -90}, {
+        $(this).animate({borderSpacing: -180}, {
             step: function (now, fx) {
                 $(this).css('-webkit-transform', 'rotate(' + now + 'deg)');
                 $(this).css('-moz-transform', 'rotate(' + now + 'deg)');
@@ -30,7 +38,7 @@ $(document).ready(function () {
             },
             duration: 'slow'
         }, 'linear');
-    })
+    });
 });
 
 
